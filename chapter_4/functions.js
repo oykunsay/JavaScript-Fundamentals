5e5f20cdc675a9ae35936ab4fa8594ea83b72f70
Öykü Ünsay <oykunsay@gmail.com>
2025-07-23 14:55:04 +0300

    lesson 4

---
+++ chapter_4/functions.js
@@ -0,0 +1,12 @@
+//function declaration
+function greet(){
+    console.log('hello there!')
+}
+
+//function expression
+const speak = function(){
+    console.log('good day!');
+};
+
+greet();
+speak();
